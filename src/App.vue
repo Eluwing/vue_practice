f8392432792be4e511f713db16960a434f39f4a0
Eluwing <abek0403@gmail.com>
2024-08-11 16:22:23 +0900

    [ Feture ] v-for文を利用してメニュー・リスト追加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
+  <div class="menu">
+    <a v-for="name in menus" :key="name">{{name}}</a>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="(name, i) in products" :key="i">
+    <h4>{{ name }}</h4>
+    <p>{{ i * 10 }}</p>
+    <button @click="repoertNumIncrease(i)">허위매물신고</button>
+    <span>신고수 : {{ productsReportNum[i] }}</span>
+  </div>
+  <!-- <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>70만원</p>
+  </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menus : ['Home', 'Product', 'About'],
+      productsReportNum : [],
+    }
+  },
+  mounted() {
+    this.productsReportNum = Array(this.products.length).fill(0)
+  },
   components: {
-    HelloWorld
-  }
+  },
+  methods : {
+    repoertNumIncrease(index){
+      this.productsReportNum[index] += 1
+    }
+  },
 }
 </script>
 
@@ -22,5 +54,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
