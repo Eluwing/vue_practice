f4c8ccf5ae595e686fd3933b8418ee1ff3d52759
Eluwing <abek0403@gmail.com>
2024-08-12 16:46:05 +0900

    [Feature] モックデータ追加

---
+++ src/assets/oneroom.js
@@ -0,0 +1,42 @@
+export default [{
+  id : 0,
+  title: "シンリム駅から30メートルの距離",
+  image: "https://codingapple1.github.io/vue/room0.jpg",
+  content: "2018年新築工事の南向きワンルーム ☀️、空気清浄機提供",
+  price: 3.4
+  },
+  {
+  id : 1,
+  title: "チャンドンオーロラ寝室(クイーンサイズ)",
+  image: "https://codingapple1.github.io/vue/room1.jpg",
+  content: "寝室のみ別にある共有シェアハウスです。最大2人可能",
+  price: 4.5
+  },
+  {
+  id : 2,
+  title: "金山アパートフラット",
+  image: "https://codingapple1.github.io/vue/room2.jpg",
+  content: "金山五叉路駅近アパートです。ペット不可 🐕",
+  price: 7.8
+  },
+  {
+  id : 3,
+  title: "ダブルベッドスタイルのスタジオアパート",
+  image: "https://codingapple1.github.io/vue/room3.jpg",
+  content: "ムアムドン近くの2人用ワンルームです。全世への転換可能",
+  price: 5.5
+  },
+  {
+  id : 4,
+  title: "明一アパートフラット",
+  image: "https://codingapple1.github.io/vue/room4.jpg",
+  content: "タンチョンドンのアパート月賃、南向き、駅から5分、虚偽物件ではありません",
+  price: 6.8
+  },
+  {
+  id : 5,
+  title: "バンジハワンルーム",
+  image: "https://codingapple1.github.io/vue/room5.jpg",
+  content: "バンジハワンルームです。雨の日に水が少し漏れる以外は良いです",
+  price: 3.7
+}];
