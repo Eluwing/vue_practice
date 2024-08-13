15dc67677e55f85df6f54084b71c88fc8dd206a5
Eluwing <abek0403@gmail.com>
2024-08-13 17:39:10 +0900

    [Feature] Modalコンポーネントリファクタリング

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="black-bg" v-if="isOpenModal">
-    <div class="white-bg">
-      <h4>{{oneRooms[modalClickedIndex].title}}</h4>
-      <p>{{oneRooms[modalClickedIndex].content}}</p>
-      <p>{{ oneRooms[modalClickedIndex].price }} 万円</p>
-      <!-- Fix me: 現在、コンポーネント化により@clickが動かない -->
-      <!-- <button @click="toggleModal">戻る</button> -->
-      <Discount/>
-    </div>
+  <div class="white-bg">
+    <h4>{{ room.title }}</h4>
+    <p>{{ room.content }}</p>
+    <p>{{ room.price }} 万円</p>
+    <!-- Fix me: 現在、コンポーネント化により@clickが動かない -->
+    <!-- <button @click="toggleModal">戻る</button> -->
   </div>
 </template>
 
 <script>
 export default {
-  name : 'ModalItem',
-  props : {
-    oneRooms : Array,
-    isOpenModal : Boolean,
-    modalClickedIndex : Number,
-
-  }
-}
+  name: "ModalItem",
+  props: {
+    room: Object,
+    isOpenModal: Boolean,
+    modalClickedIndex: Number,
+  },
+};
 </script>
 
 <style>
-
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 </style>
