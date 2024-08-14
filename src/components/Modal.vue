07345912b33d5c41b30e36c3ebeb84123ff8c93e
Eluwing <abek0403@gmail.com>
2024-08-14 16:44:01 +0900

    [Feature] 入力ボックス追加

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,7 +2,10 @@
   <div class="white-bg">
     <h4>{{ room.title }}</h4>
     <p>{{ room.content }}</p>
-    <p>{{ room.price }} 万円</p>
+    <!-- inputからデータ受ける方法：@input、v-model利用 -->
+    <!-- <input @input = "month = $event.target.value" /> -->
+    <input v-model.number="month" />
+    <p>{{ month }}ヶ月選択 : {{ room.price * month }} 万円</p>
     <button @click="$emit('closeModal')">戻る</button>
   </div>
 </template>
@@ -10,6 +13,11 @@
 <script>
 export default {
   name: "ModalItem",
+  data(){
+    return {
+      month: 0,
+    }
+  },
   props: {
     room: Object,
     isOpenModal: Boolean,
