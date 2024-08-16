06d7f9996dca13adaa0d276e2b16c0f8e1f6b422
Eluwing <abek0403@gmail.com>
2024-08-16 15:55:07 +0900

    [feature] パーセンテージが1秒ごとに１ずつ減少するInterval追加

--- src/components/Discount.vue
+++ src/components/Discount.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="discount">
-    <h4>今新規登録すると20%割り引き</h4>
+    <h4>今新規登録すると{{salePercent}}%割り引き</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'DiscountItem',
+  data() {
+    return {
+      salePercent: 30,
+    }
+  },
+  mounted() {
+    const salePercnetDesent = setInterval(() => {
+      if (this.salePercent > 0) {
+        this.salePercent -= 1;
+      } else {
+        clearInterval(salePercnetDesent); // パーセンテージが0になった場合、Interval中止
+      }
+    }, 1000); // 1秒ごとに実行
+  }
 }
 </script>
 
